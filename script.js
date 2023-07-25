ca0bb5ce6481d54c103fddb96fde1c2ac7ed939e
Rahul Khandait <90828823+rahulkhandait-sde@users.noreply.github.com>
2023-07-26 04:38:54 +0530

    file updated

--- script.js
+++ script.js
@@ -3,20 +3,50 @@ let buttons = document.querySelectorAll('button');
 
 let string = '';
 let arr = Array.from(buttons);
+let lastInputIsOperator = false;
+let lastOperator = null;
+
 arr.forEach((button) => {
 	button.addEventListener('click', (e) => {
-		if (e.target.innerHTML == '=') {
-			string = eval(string);
-			input.value = string;
-		} else if (e.target.innerHTML == 'AC') {
+		const currentInput = e.target.innerHTML;
+
+		if (currentInput === '=') {
+			if (!lastInputIsOperator) {
+				string = eval(string);
+				input.value = string;
+			}
+		} else if (currentInput === 'AC') {
 			string = '';
 			input.value = string;
-		} else if (e.target.innerHTML == 'DEL') {
+			lastInputIsOperator = false;
+			lastOperator = null;
+		} else if (currentInput === 'DEL') {
 			string = string.substring(0, string.length - 1);
 			input.value = string;
+			lastInputIsOperator = isOperator(string[string.length - 1]);
+			lastOperator = null;
 		} else {
-			string += e.target.innerHTML;
+			if (lastInputIsOperator && isOperator(currentInput)) {
+				// Do not add consecutive operators or start with an operator
+				lastOperator = currentInput;
+				if (string.length - 1 >= 0) {
+					string = string.substring(0, string.length - 1) + currentInput;
+				}
+			} else if (string === '' && isOperator(currentInput)) {
+				return;
+			} else {
+				string += currentInput;
+				lastOperator = null;
+			}
+
 			input.value = string;
+			lastInputIsOperator = isOperator(currentInput);
 		}
 	});
 });
+
+function isOperator(value) {
+	// Define the list of operators
+	const operators = ['+', '-', '*', '/', '%'];
+	return operators.includes(value);
+}
